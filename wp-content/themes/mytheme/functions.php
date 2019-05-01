556a17e677177828970919df6201dff0b726ff6e
thinkingbox <thinkingbox@thinkingboxsmbp.lan>
2019-05-01 17:06:35 -0400

    enque jquery

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,11 +1,42 @@
 <?php
 
+
+function include_jquery(){
+
+    wp_deregister_script('jquery');
+
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.4.0.min.js', '', 1, true);
+  
+
+    add_action('wp_enqueue_scripts','jquery');
+}
+
+add_action('wp_enqueue_scripts','include_jquery');
+
+
+
+
+function loadjs(){
+
+    wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
+    wp_enqueue_script('customjs');
+}
+
+add_action('wp_enqueue_scripts','loadjs');
+
+
+
+
+
 function load_stylesheets(){
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootstrap');
 
-    wp_enqueue_style('stylesheet');
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
+    wp_enqueue_style('style');
 
 } 
 
 add_action('wp_enqueue_scripts','load_stylesheets');
+
