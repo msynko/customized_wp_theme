80ce3ec8636157fd015b461cc249f800b0d3d2a4
thinkingbox <thinkingbox@thinkingboxsmbp.lan>
2019-05-01 16:23:46 -0400

    enque stylesheet

---
+++ wp-content/themes/mytheme/front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col">Left side
+            </div>
+
+            <div class="col">Right side
+            </div>
+    </div>
+
+</div>
+
+<?php get_footer();?>
